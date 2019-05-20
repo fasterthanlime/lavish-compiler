a468b06dc735c3744e568fe11b3a70483bdeffb7
Amos Wenger <amoswenger@gmail.com>
2019-05-20 15:02:02 +0200

    remove unnecessary trait

--- lavish-rpc/src/system.rs
+++ lavish-rpc/src/system.rs
@@ -402,8 +402,3 @@ where
         self.in_flight_requests.get(&id).map(|req| req.method)
     }
 }
-
-pub trait IntoHandleRef<P, NP, R>
-where P: Atom, NP: Atom, R: Atom {
-    fn into_handle_ref<'a>() -> &'a mut Handle<P, NP, R>;
-}
