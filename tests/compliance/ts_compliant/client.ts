065d155fb154c0ff4ac2cafe362aca4e7ce27472
Amos Wenger <amoswenger@gmail.com>
2019-06-28 18:59:02 +0200

    Stub out ts_compliant

---
+++ tests/compliance/ts_compliant/client.ts
@@ -0,0 +1,4 @@
+
+export default async function client(_address: string) {
+    console.log(`Should run client...`);
+}
---
+++ tests/compliance/ts_compliant/server.ts
@@ -0,0 +1,5 @@
+
+export default async function server() {
+    console.log(`Should run server...`);
+}
+
